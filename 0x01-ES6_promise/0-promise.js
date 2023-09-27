95303ddd18f140c99b3ec89bb06fb449c58c4d87
kimenyu <njorogekimenyu@gmail.com>
2023-09-27 05:54:36 +0000

    0-

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,5 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const responseData = "API response data";
-        resolve(responseData);
-      }, 1000);
-    });
-  }
+  return new Promise((resolve, reject) => {
+    resolve();
+  });
+}
