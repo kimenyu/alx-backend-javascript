b3972d04d109e95ef38c68924e817c441dd712f6
kimenyu <njorogekimenyu@gmail.com>
2023-09-28 03:00:59 +0000

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  return promise
-    .then((result) => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
+    return new Promise((resolve, reject) => {
+        if (promise) {
+            resolve({status:200, body: 'Success'});
+            console.log('Got a response from the API')
+        } else {
+            reject();
+            console.log('Got a response from the API');
+        }
     })
-    .catch((error) => {
-      console.log('Got a response from the API');
-      return new Error(); 
-    });
 }
