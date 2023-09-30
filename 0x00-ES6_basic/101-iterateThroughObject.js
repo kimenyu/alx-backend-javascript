a7ab55197083e3bf57fffd341837fef41d69c933
kimenyu <njorogekimenyu@gmail.com>
2023-09-30 10:01:55 +0000

    rem

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++
@@ -1,9 +0,0 @@
-export default function iterateThroughObject(reportWithIterator) {
-  const employeesArray = [];
-  
-  for (const employee of reportWithIterator) {
-    employeesArray.push(employee);
-  }
-  
-  return employeesArray.join(' | ');
-}
