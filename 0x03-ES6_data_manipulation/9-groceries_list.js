06e6b4f860036a5fa538a0bc803c1589de2e4315
kimenyu <njorogekimenyu@gmail.com>
2023-10-03 09:45:03 +0000

    9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const myGroceries = new Map();
+  myGroceries.set('Apples', 10);
+  myGroceries.set('Tomatoes', 10);
+  myGroceries.set('Pasta', 1);
+  myGroceries.set('Banana', 5);
+
+  return myGroceries;
+}
