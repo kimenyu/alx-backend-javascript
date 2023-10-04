82342d8adf6ae601959695af3207a6d91ac2011b
kimenyu <njorogekimenyu@gmail.com>
2023-10-04 08:41:00 +0000

    rem

--- 0x01-ES6_promise/4-user-promise.js
+++
@@ -1,6 +0,0 @@
-export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve, reject) => {
-    resolve({ firstName, lastName });
-    reject();
-  });
-}
--- 0x01-ES6_promise/5-photo-reject.js
+++
@@ -1,6 +0,0 @@
-export default function uploadPhoto(fileName) {
-  return new Promise((resolve, reject) => {
-    const error = new Error(`${fileName} cannot be processed`);
-    reject(error);
-  });
-}
