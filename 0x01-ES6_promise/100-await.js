0e752ed9ec8db9eb3ecde0162276e8d0a4bf5a29
kimenyu <njorogekimenyu@gmail.com>
2023-10-13 14:44:20 +0000

    100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,12 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const all = Promise.all([uploadPhoto(), createUser()]).then((values) => {
-    const photo = values[0];
-    const user = values[1];
+  const all = Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const photo = values[0];
+      const user = values[1];
 
-    return { photo, user };
-  })
+      return { photo, user };
+    })
     .catch((error) => error);
 
   const data = await all;
