cc7b26dae4a6b663d54189af5d84b7741bfb2948
kimenyu <njorogekimenyu@gmail.com>
2023-10-13 18:16:26 +0000

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  c1 = new ClassRoom(19);
-  c2 = new ClassRoom(20);
-  c3 = new ClassRoom(34);
+  const c1 = new ClassRoom(19);
+  const c2 = new ClassRoom(20);
+  const c3 = new ClassRoom(34);
   myArray = [c1, c2, c3];
   return myArray;
 }
