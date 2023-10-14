df4a0b3cccdc6371e69ae542756d2ca00dfa020b
kimenyu <njorogekimenyu@gmail.com>
2023-10-14 13:01:17 +0000

    7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,7 +4,23 @@ export default class Airport {
     this._code = code;
   }
 
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
   toString() {
-    return `${this._code}`;
+    return `[object ${this.code}]`;
   }
 }
